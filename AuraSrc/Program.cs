e1d1adb00bb6a4d07ab5920beab19c787316ebb6
paulica fara frica <49493490+killpillp@users.noreply.github.com>
2021-03-21 20:47:07 +0200

    Add files via upload

---
+++ AuraSrc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Colorful;
+using PaulModz.Core;
+using PaulModz.Core.Network;
+using PaulModz.Core.Settings;
+
+namespace PaulModz
+{
+    internal class Program
+    {
+        internal static Stopwatch elpst = new Stopwatch();
+
+        private static void Main(string[] args)
+        {
+            Console.Write(@"
+           ██████╗░░█████╗░██╗░░░██╗██╗░░░░░███╗░░░███╗░█████╗░██████╗░███████
+           ██╔══██╗██╔══██╗██║░░░██║██║░░░░░████╗░████║██╔══██╗██╔══██╗╚════██║
+           ██████╔╝███████║██║░░░██║██║░░░░░██╔████╔██║██║░░██║██║░░██║░░███╔═╝ 
+           ██╔═══╝░██╔══██║██║░░░██║██║░░░░░██║╚██╔╝██║██║░░██║██║░░██║██╔══╝░░
+           ██║░░░░░██║░░██║╚██████╔╝███████╗██║░╚═╝░██║╚█████╔╝██████╔╝███████╗ 
+           ╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚══════╝ v." +
+                          Constants.ServerConfig.AppVersion, Color.Orange);
+            Console.WriteLine();
+
+            elpst.Reset();
+
+            elpst.Start();
+
+            Logger.Log($"Se pregateste...", Logger.DefCon.INFO);
+
+            AsynchronousSocketListener.StartListening();
+
+            Console.ReadKey(true);
+        }
+    }
+}
